e07e669198e0b2ce6432425bc5eab8f582a34a63
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-02-18 15:28:55 +0200

    Core domain entities

---
+++ src/Core/HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ src/Core/HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
