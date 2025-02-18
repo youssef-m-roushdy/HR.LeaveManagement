4d22e34a36c83f652b71ac0e75ac93861118746f
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-02-18 18:38:15 +0200

    Assign cotracts

---
+++ src/Core/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+    {
+        
+    }
+}
