4808a18ec4e1cb9ad5f39033a19e273bb1dcf553
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-02-20 20:16:01 +0200

    Adding entity framework core

---
+++ src/InfraStructure/HR.LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options)
+        : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveType> LeaveType { get; set; }
+        public DbSet<LeaveRequest> LeaveRequest { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocation { get; set; }
+    }
+}
